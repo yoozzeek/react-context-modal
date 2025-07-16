6de39f32dfd051be4c4721dd582cc63105af2aa5
25x8 <kocherginji@gmail.com>
2025-07-16 10:31:09 +0700

    ADD: build:demo npm command; update vite config to support vite dev server, and both building library and static demo for github pages

--- lib/hooks/useModal.ts
+++ lib/hooks/useModal.ts
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { ModalContext } from "../providers/ModalProvider";
-import type { StackCtx } from "@/types.ts";
+import type { StackCtx } from "@/types";
 
 export default function useModal(fallbackCtx?: StackCtx) {
   const ctx = useContext(ModalContext);
--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,13 @@ import react from "@vitejs/plugin-react";
 import dts from "unplugin-dts/vite";
 import svgr from "vite-plugin-svgr";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
-import path from "node:path";
 import { fileURLToPath } from "node:url";
+import path from "node:path";
 import type { PluginOptions } from "unplugin-dts";
+import type { BuildEnvironmentOptions } from "vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const isLibBuild = process.env.NODE_ENV === "production";
 
 const dtsOptions = {
   exclude: ["**/*.stories.tsx"],
@@ -20,30 +19,40 @@ const dtsOptions = {
 } as PluginOptions;
 
 // https://vite.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./lib"),
+export default defineConfig(({ mode }) => {
+  const isDemo = mode === "demo";
+  const isDev = process.env.NODE_ENV === "development";
+  const buildExamples = isDemo || isDev;
+  return {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./lib"),
+      },
     },
-  },
-  plugins: [react(), svgr(), libInjectCss(), dts(dtsOptions)],
-  root: isLibBuild ? "." : "examples",
-  build: isLibBuild
-    ? {
-        lib: {
-          entry: path.resolve(__dirname, "lib/index.ts"),
-          name: "react-context-modal",
-          formats: ["es"],
-        },
-        rollupOptions: {
-          external: [
-            "react",
-            "react-dom",
-            "react/jsx-runtime",
-            "react-responsive",
-            "simplebar-react",
-          ],
-        },
-      }
-    : undefined,
+    plugins: [react(), svgr(), ...(isDemo ? [] : [libInjectCss(), dts(dtsOptions)])],
+    root: buildExamples ? "examples" : ".",
+    build: isDemo
+      ? ({
+          outDir: path.resolve(__dirname, "./dist"),
+          emptyOutDir: true,
+        } as BuildEnvironmentOptions)
+      : isDev
+        ? undefined // no need to build with vite dev server
+        : {
+            lib: {
+              entry: path.resolve(__dirname, "lib/index.ts"),
+              name: "react-context-modal",
+              formats: ["es"],
+            },
+            rollupOptions: {
+              external: [
+                "react",
+                "react-dom",
+                "react/jsx-runtime",
+                "react-responsive",
+                "simplebar-react",
+              ],
+            },
+          },
+  };
 });
