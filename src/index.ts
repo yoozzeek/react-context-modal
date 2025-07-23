958db6f9bca7cbc5fc49766fe0c094de04f24255
Andrew <zeek@tuta.com>
2025-07-23 10:37:48 +0700

    BUG: ModalProvider is not exported

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as Modal } from "@/components/Modal";
-export { default as useModal } from "@/hooks/useModal.ts";
-export { default as useModalStackCtx } from "@/hooks/useModalStackCtx.ts";
+export { default as ModalProvider } from "@/providers/ModalProvider";
+export { default as useModal } from "@/hooks/useModal";
+export { default as useModalStackCtx } from "@/hooks/useModalStackCtx";
 export type { ModalType, StackCtx, Size } from "@/types";
