219b6aa7130d79aa36cae1f9b5685591ed320484
Andrew <zeek@tuta.com>
2025-07-29 10:17:16 +0700

    FIX: imports in examples

--- examples/WithCreateStackHook.tsx
+++ examples/WithCreateStackHook.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Modal, useModalStackCtx } from "../src";
 
 const WithCreateStackHookExample = () => {
--- examples/WithFooterPortal.tsx
+++ examples/WithFooterPortal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Modal } from "../src";
 import ModalProvider from "../src/providers/ModalProvider";
 import ModalFooterPortal from "../src/components/ModalFooterPortal";
